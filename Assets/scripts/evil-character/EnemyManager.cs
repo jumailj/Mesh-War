7053162c4b3006f195ba9cb99ffe9df8f8a10685
jumail j <jumailj.work@gmail.com>
2022-12-05 22:40:42 +0530

    added evil character controller script

---
+++ Assets/scripts/evil-character/EnemyManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    // enemy object rotation 
+    [Range(50.0f, 180.0f)]
+    public float RotationSpeed = 50.0f;
+
+    //set bullet speed
+    [Range(10,50)]
+    public float bulletSpeed = 10.0f;
+
+    // it is used to get the postion&rotation to spawn a bullet
+    public Transform SpawnTransform;
+
+    //bullet prefab object;
+    public GameObject bullet;
+
+    // bullet speed controller;
+    public bullet bulletscript;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // object rotation;
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime); 
+
+        //draw Red ray(Debug)
+        Vector3 forwardDistance = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forwardDistance, Color.red);
+
+        //spawn bullet;
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(bullet, SpawnTransform.position, SpawnTransform.rotation);
+        }
+    }
+}
