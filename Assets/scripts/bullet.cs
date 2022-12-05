0a4c2ff66a7ec5104ae227f5b635c4c11e9478d6
jumail j <jumailj.work@gmail.com>
2022-12-05 13:05:07 +0530

    added evil character

---
+++ Assets/scripts/bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject parent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward* 10 * Time.deltaTime);    
+    }
+}
